bb4a5058709243dff9f5f26e81ceb751407db49b
Janluiz <103154512+Janluiz@users.noreply.github.com>
2022-10-18 15:45:24 -0300

    EX SIMPLES SOMA

---
+++ EXsoma.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class EXsoma {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x , y, z;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		z = x + y;
+		
+		System.out.println("SOMA = " + z);
+		
+		
+		
+		
+	}
+
+}
--- Exercicios/Main.java
+++ Exercicios/Main.java
@@ -1,3 +1,4 @@
+package Exercicios;
 import java.util.Locale;
 public class Main {
 
